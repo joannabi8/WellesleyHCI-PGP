a07f2c42f5aa7cc99da28e950371061145921b86
Joanna Bi <jbi@wellesley.edu>
2014-06-09 13:08:17 -0400

    fixing syntax errors

--- pretask_process.php
+++ pretask_process.php
@@ -7,27 +7,25 @@ SUMMER 2014
 <?php
 session_start();
 error_reporting(E_ERROR);
-
-require_once('pgp_function.php');
+require_once('pgp_functions.php');
 $dbh;
 localConn();
 
 // HELPER FUNCTION: Insert new user response from pretask page
 function add_pretask_row($userResponseValues) {
 	global $dbh;
-	$query = "INSERT INTO NEW_PRETASK VALUES (DEFAULT,?,?,?,?,?,?,?)";
+	$query = "INSERT INTO NEW_PRETASK VALUES (DEFAULT,?,?,?,?,?,?)";
 	return $result = prepared_query($dbh, $query, $userResponseValues);
 }
 
 // PROCESS DATA
 if (!empty($_POST)) {
 	$pretaskResponse = getUserResponse($_POST);
-
 	$id = $_SESSION["user"];
-	$ip = $_SERVER["REMOTE_ADDR"] //documented PHP var
-
+	$ip = $_SERVER["REMOTE_ADDR"]; //documented PHP var
+	echo "HERE";
 	// Filter out used ip's
-	$ipUsed = filter_var($ip, FILTER_VALIDATE_IP) ? check_ip($ip) : true; //more concise 'if' clause
+	$ipUsed = filter_var($ip, FILTER_VALIDATE_IP) ? ip_exists($ip) : true; //more concise 'if' clause
 
 	if (!$ipUsed) {
 		add_pretask_row($pretaskResponse);
@@ -35,16 +33,16 @@ if (!empty($_POST)) {
 		$add_user = "INSERT INTO NEW_USER VALUES (?,?,?,?,?,?,?,?)";
 		prepared_query($dbh, $add_user, array($id, $pretask_id, NULL, NULL, NULL, NULL, NULL, NULL));
 	}
-
+	
 	// Redirect user to a random visualization
 	$num = mt_rand(1,4); //documented php function
 	$header = "Location: http://cs.wellesley.edu/~hcilab/pghci_NEW/";
-	if ($num=1) { $header = $header . "v1.php"; }
-	else if ($num=2) { $header = $header . "v2.php"; }
-	else if ($num=3) { $header = $header . "v3.php"; }
+	if ($num == 1) { $header = $header . "v1.php"; }
+	else if ($num == 2) { $header = $header . "v2.php"; }
+	else if ($num == 3) { $header = $header . "v3.php"; }
 	else { $header = $header . "v4.php"; }
    	header($header); //redirects user
-    exit();
+	exit();
 }
 	
 ?>
