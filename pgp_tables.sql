7b54b43890779db107f313ce304eeadb41ece8d0
Joanna Bi <jbi@wellesley.edu>
2014-06-11 13:39:42 -0400

    Syncing
    Github won't let me sync anymore without committing? Thanks git.

--- pgp_tables.sql
+++ pgp_tables.sql
@@ -3,7 +3,6 @@ use pgvis_for_get;
 drop table if exists NEW_USER;
 drop table if exists NEW_PRETASK;
 drop table if exists NEW_VIS;
-drop table if exists NEW_SHARING;
 drop table if exists NEW_DEMOGRAPHS;
 
 -- create the user table
@@ -17,7 +16,8 @@ create table NEW_USER(
 	barchart_id int,
 	sharing_id int,
 	demo_id int,
-	ip varchar(50)
+	ip varchar(50),
+	total_time int
 	)
 	ENGINE = InnoDB;
 
@@ -29,7 +29,8 @@ create table NEW_PRETASK(
 	q3 enum('true','false') not null,
 	q4 enum('true','false') not null,
 	q5 enum('true','false') not null,
-	q6 enum('yes','no') not null
+	q6 enum('yes','no') not null,
+	time_spent int
 	)
 	ENGINE = InnoDB;
 
@@ -54,7 +55,7 @@ create table NEW_VIS(
 	q9_g char(12) not null,
 	q9_h char(11) not null,
 	q10_a enum('1','2','3','4','5','6','7') not null,
-    q10_b enum('1','2','3','4','5','6','7') not null,
+	q10_b enum('1','2','3','4','5','6','7') not null,
     q10_c enum('1','2','3','4','5','6','7') not null,
     q10_d enum('1','2','3','4','5','6','7') not null,
     q10_e enum('1','2','3','4','5','6','7') not null,
@@ -66,14 +67,8 @@ create table NEW_VIS(
     q10_k enum('1','2','3','4','5','6','7') not null,
     q10_l enum('1','2','3','4','5','6','7') not null,
     q10_m enum('1','2','3','4','5','6','7') not null,
-    q11 varchar(500)
-	)
-	ENGINE = InnoDB;
-
--- create the sharing table
-create table NEW_SHARING(
-	sharing_id int not null primary key
-
+    q11 varchar(500),
+    time_spent int
 	)
 	ENGINE = InnoDB;
 
@@ -86,6 +81,7 @@ create table NEW_DEMOGRAPHS(
 	q4 enum('female','male','other'),
 	q5 char(20), -- how it's done in old db, not sure i understand...
 	q6 enum('yes','no'),
-	q7 enum('yes','no')
+	q7 enum('yes','no'),
+	time_spent int
 	)
 	ENGINE = InnoDB;
